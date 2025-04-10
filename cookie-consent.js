57647fd3bd26bd9fbdfa3f6001c80a9bafd5c6ff
reshmalb17 <reshma@seattlenewmedia.com>
2025-04-10 19:46:35 +0000

    Update cookie-consent.js

--- cookie-consent.js
+++ cookie-consent.js
@@ -86,7 +86,17 @@ const EncryptionUtils = {
     }
   };
 
-  
+  async function generateKey() {
+    const key = await crypto.subtle.generateKey(
+      { name: "AES-GCM", length: 256 },
+      true,
+      ["encrypt", "decrypt"]
+    );
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+    const exportedKey = await crypto.subtle.exportKey("raw", key);
+    return { secretKey: exportedKey, iv };
+  } 
+
   async function encryptData(data, key, iv) {
     const encoder = new TextEncoder();
     const encodedData = encoder.encode(data);
